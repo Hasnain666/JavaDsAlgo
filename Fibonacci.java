9144882ce765fc9a05efc9848405527cfa1265e0
Hasnain Arif Thara <hasnainthara786@gmail.com>
2022-10-28 21:30:50 +0530

    added fibonacci program

---
+++ Fibonacci.java
@@ -0,0 +1,21 @@
+//Program to print nth fibonacci number
+package com.hasnain;
+
+import java.util.Scanner;
+
+public class FIbonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        int count = 2;
+        while (count<=n){
+            int temp = b;
+            b = b + a;
+            a = temp;
+            count++;
+        }
+        System.out.println(b);
+    }
+}
