5b3863e5295043bd00afcdffff2aa3807768de6a
Hasnain Arif Thara <hasnainthara786@gmail.com>
2022-10-28 18:43:30 +0530

    added CaseCheck Program

---
+++ CaseCheck.java
@@ -0,0 +1,19 @@
+//Program to check if letter added by the user is uppercase or lowercase
+package com.hasnain;
+
+import java.util.Scanner;
+
+public class CaseCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().trim().charAt(0);
+        if (ch > 'a' && ch < 'z'){
+            System.out.println("LowerCase");
+        }
+        else{
+            System.out.println("UpperCase");
+        }
+
+
+    }
+}
