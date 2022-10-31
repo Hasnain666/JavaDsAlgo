080eb2c12b9413da1da9d1a435e358ea643e250a
Hasnain Arif Thara <hasnainthara786@gmail.com>
2022-10-31 16:54:31 +0530

    added searchInString

---
+++ searching/searchInString.java
@@ -0,0 +1,23 @@
+//Linear Search in String
+package com.hasnain;
+
+public class searchInString {
+    public static void main(String[] args) {
+        String name = "Hasnain";
+        char target = 'a';
+        System.out.println(search(name,target));
+
+    }
+    static boolean search(String name, char target){
+        if (name.length()==0){
+            return false;
+        }
+        for (int i = 0; i < name.length(); i++){
+            if (target==name.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
